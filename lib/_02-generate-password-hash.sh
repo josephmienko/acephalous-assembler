bbb42b301696122a7ebd8c99125ea73a1e6a1987
Joseph A. Mienko <mienko@uw.edu>
2026-04-15 22:40:22 -0700

    style: improve password hash script user prompts

--- lib/_02-generate-password-hash.sh
+++ lib/_02-generate-password-hash.sh
@@ -8,14 +8,12 @@ OPENSSL_BIN="$(brew --prefix openssl@3)/bin/openssl"
 touch "$CONFIG_FILE"
 
 if ! grep -q '^PASSWORD_HASH=' "$CONFIG_FILE"; then
-  printf "Enter the password to hash for the Ubuntu installer: " >&2
+  printf "Password: " >&2
   IFS= read -r -s PASSWORD
   printf "\n" >&2
-
   PASSWORD_HASH="$("$OPENSSL_BIN" passwd -6 "$PASSWORD")"
   printf "PASSWORD_HASH='%s'\n" "$PASSWORD_HASH" >> "$CONFIG_FILE"
-
-  echo "Generated new PASSWORD_HASH and saved to $CONFIG_FILE"
+  echo "Generated new PASSWORD_HASH and saved to config.env"
 else
   echo "PASSWORD_HASH already exists. Using persisted value."
 fi
